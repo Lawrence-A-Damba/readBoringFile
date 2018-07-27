d30d72dbbdcf0c3c8233c7654b184b87fdf6a845
Lawrence <dambapls@gmail.com>
2018-07-27 19:57:17 +0000

    Modified rbf.py.

--- rbf.py
+++ rbf.py
@@ -1,13 +1,14 @@
-import os, fnmatch
+import os
+import fnmatch
 boringFileNames = []
 listOfFiles = os.listdir('.')
 pattern = "*.TXT"
 for entry in listOfFiles:
-	if fnmatch.fnmatch(entry, pattern):
-		boringFileNames.append(entry)
-print('#',boringFileNames)
+    if fnmatch.fnmatch(entry, pattern):
+        boringFileNames.append(entry)
+print('#', boringFileNames)
 for boringName in boringFileNames:
-	open_file=open(boringName,'r')
-	file=open_file.readlines()
-	print(file)
+    open_file = open(boringName, 'r')
+    file = open_file.readlines()
+    print(file)
 open_file.close()
