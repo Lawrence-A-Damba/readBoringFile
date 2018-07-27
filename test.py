c366c59c88bc4451be07cf7e6fab7ecfa0293b4a
Lawrence <dambapls@gmail.com>
2018-07-27 22:44:35 +0000

    End of the day.

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+boringFileNames = []
+listOfFiles = os.listdir('.')
+newList = []
+for names in listOfFiles:
+    if names.endswith(".txt"):
+       newList.append(names)
+    if names.endswith(".TXT"):
+       newList.append(names)   
+for boringName in newList:
+    print('\n\n\nThis is the boring name, ', boringName, '\n\n\n')
+    open_file = open(boringName, 'r')
+    file = open_file.readlines()
+    print(file)
+open_file.close()
