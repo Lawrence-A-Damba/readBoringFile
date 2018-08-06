cc066d59cad4f88860e3daa5e9e30b24d054c594
Lawrence A. Damba <dambapls@gmail.com>
2018-08-06 13:56:21 -0500

    modified rbf.py

--- rbf.py
+++ rbf.py
@@ -10,5 +10,15 @@ print('#', boringFileNames)
 for boringName in boringFileNames:
     open_file = open(boringName, 'r')
     file = open_file.readlines()
-    print(file)
+#    print(file)
 open_file.close()
+print(file[0:2])
+text=str(file[8])
+text=text.split(' ')
+print(text)
+for item in text:
+    if item == '':
+        text.remove('')
+    else:
+        pass
+print(text)
